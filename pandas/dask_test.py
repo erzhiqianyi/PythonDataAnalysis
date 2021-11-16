a1357a7447c4f90c4da2d95de1fbf292380ec4f4
二之前一 <github@caofeng.me>
2021-11-16 23:02:18 +0800

    test read dask csv

--- pandas/dask_test.py
+++ pandas/dask_test.py
@@ -12,15 +12,27 @@ def read_csv(path):
     return dd.read_csv(path)
 
 
+def apply_with_dict(df, dict_value):
+    for key in dict_value:
+        value = dict_value[key]
+        df[key] = value
+    return df
+
+
 def write_to_csv(df):
     df.to_csv('movie_dask.csv')
+
 
 if __name__ == '__main__':
     client = Client()
     column_map = {'column': 'director_usd', 'source': 'director_name'}
     dfs = client.map(read_csv, ['movie.csv'])
     dfs = client.map(copy_columns, dfs, [column_map])
-    l = client.map(write_to_csv,dfs)
+    dict_value = {
+        'A': 'TEST'
+    }
+    dfs = client.map(apply_with_dict, dfs, [dict_value])
+    l = client.map(write_to_csv, dfs)
     client.submit(len, l).result()
 
     print("start")
